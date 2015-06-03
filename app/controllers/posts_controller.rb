effd9fb47fc66e92bf1c57da91e6d353c8ddf0fa
Mark Azemoon <mmazemoon@gmail.com>
2015-06-03 11:55:13 -0700

    build post model

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,53 @@
+class PostsController < ApplicationController
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+
+    if @post.save
+      render json: @post
+    else
+      render json: @post.errors.full_messages, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+
+    if @post.update(post_params)
+      render json: @post
+    else
+      render json: @post.errors.full_messages, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+
+    render json: @post
+  end
+
+  def index
+    @posts = Post.all
+
+    render json: @posts
+  end
+
+  def show
+    @post = Post.find(params[:id])
+    render json: @post
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :body)
+  end
+end
