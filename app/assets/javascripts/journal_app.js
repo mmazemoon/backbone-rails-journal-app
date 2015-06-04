88789fca4fbe3859e00b60573fd9e992afa44c81
Mark Azemoon <mmazemoon@gmail.com>
2015-06-03 18:10:55 -0700

    Replace main div with content & sidebar divs

--- app/assets/javascripts/journal_app.js
+++ app/assets/javascripts/journal_app.js
@@ -5,7 +5,13 @@ window.JournalApp = {
   Views: {},
   Routers: {},
   initialize: function() {
-    var router = new JournalApp.Routers.Posts({ $rootEl: $(".main") });
+    var router = new JournalApp.Routers.Posts({ $rootEl: $("#content") });
+    var newCollection = new JournalApp.Collections.Posts();
+    newCollection.fetch();
+    var indexView = new JournalApp.Views.PostsIndex({collection: newCollection});
+
+    $("#sidebar").html(indexView.render().$el);
+
     Backbone.history.start();
   }
 };
